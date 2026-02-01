889bb2377eb87812dc0c4651b9a38caa7aef3d64
kai694966 <koutisannsuudaisukimann@gmail.com>
2026-02-01 23:11:10 +0900

    260201 バックグラウンドのjson等出力追加

--- Music_manager5.2.py
+++ Music_manager5.2.py
@@ -4,6 +4,7 @@ import os
 from datetime import datetime
 import re
 import download
+import output
 import subprocess
 
 
@@ -13,7 +14,7 @@ config = {
     "password":os.environ.get("PSQL_PASSWORD"),
     "host":"localhost",
     "port":5432,
-}#download.pyにもconfigがある
+}#download.py,output.pyにもconfigがある
 
 def connect_db(dbname):
     temp_config = config.copy()
@@ -59,7 +60,8 @@ def create_tables():
             volume_offset INTEGER DEFAULT 0,
             duration INTEGER,
             source TEXT UNIQUE,
-            language char(10) DEFAULT 'ja'
+            language char(10) DEFAULT 'ja',
+            hour INTEGER
 
         );
         """
@@ -106,71 +108,108 @@ def registration():
 
 def process_metadata_entry(db_track, index, total,type):
 
-    print(f"\n--- [{index}/{total}] {db_track['name']} ---")
-    
-    if os.path.exists(db_track['path_audio']):
-        os.startfile(db_track['path_audio'])
-    
-    type_input = type
-    
-    time_map = {"ln": "lateNight", "em": "earlyMorning", "m": "morning", "lm": "lateMorning", 
-                "ea": "earlyAfternoon", "la": "lateAfternoon", "en": "earlyNight", "n": "night", "all": "all"}
-    
-    time_input = input(f"[{index}/{total}]その音楽を再生する時間帯は？','で区切る\n0-4   lateNight\n5-6   earlyMorning\n7-9   morning\n10-12 lateMorning\n13-15 earlyAfternoon\n16-17 lateAfternoon\n18-20 earlyNight\n21-23 night\n0-23  all\n(none)none\n>>") or "all"
-    time_list = [time_map.get(t.strip().lower(), t.strip()) for t in time_input.split(",")]
+    if type == "music":
 
-    signal_time = input("その音楽を時報として流す時間(HHMM)\n[none]\n>>")
-    
-    if signal_time == "":
-        signal_time = None
+        print(f"\n--- [{index}/{total}] {db_track['name']} ---")
 
-    is_travel = input("旅行の時のみ流れる？(t/f)\n[false]\n>>")
-    if is_travel in ["t","ｔ","true","True"]:
-        is_travel = True
-        only_travel = input("旅行する時のみ流す？\n>>")
-        if only_travel in ["t","ｔ","true","True"]:
-            only_travel = True
+        if os.path.exists(db_track['path_audio']):
+            os.startfile(db_track['path_audio'])
+
+        type_input = type
+
+        time_map = {"ln": "lateNight", "em": "earlyMorning", "m": "morning", "lm": "lateMorning", 
+                    "ea": "earlyAfternoon", "la": "lateAfternoon", "en": "earlyNight", "n": "night", "all": "all"}
+
+        time_input = input(f"[{index}/{total}]その音楽を再生する時間帯は？','で区切る\n0-4   lateNight\n5-6   earlyMorning\n7-9   morning\n10-12 lateMorning\n13-15 earlyAfternoon\n16-17 lateAfternoon\n18-20 earlyNight\n21-23 night\n0-23  all\n(none)none\n>>") or "all"
+        time_list = [time_map.get(t.strip().lower(), t.strip()) for t in time_input.split(",")]
+
+        signal_time = input("その音楽を時報として流す時間(HHMM)\n[none]\n>>")
+
+        if signal_time == "":
+            signal_time = None
+
+        is_travel = input("旅行の時のみ流れる？(t/f)\n[false]\n>>")
+        if is_travel in ["t","ｔ","true","True"]:
+            is_travel = True
+            only_travel = input("旅行する時のみ流す？\n>>")
+            if only_travel in ["t","ｔ","true","True"]:
+                only_travel = True
+            else:
+                only_travel = False
         else:
             only_travel = False
-    else:
-        only_travel = False
-        is_travel = False
+            is_travel = False
 
-    # ... (signal_time, is_travel, weather などの入力は以前のコードと同じ) ...
-    weather = input("天気 (Clear, Clouds, Rain, Snow)\n>>").split(",")
-    
-    # メタデータ推論
-    s_name, s_cover, s_original = name_suggestion(db_track['name'])
-    name = input(f"その曲は何と呼ばれている？ [{s_name}] >>") or s_name
-    cover = input(f"カバーした人はだれ？ [{s_cover}] >>") or s_cover
-    original = input(f"原曲をうたったのはだれ？ [{s_original}] >>") or s_original
-    
-    today = datetime.now().strftime('%Y%m%d')
-    registered = input(f"登録日 [{today}] >>") or today
-    language = input(f"曲の言語[{db_track["language"]}]\n>>") or db_track["language"]
-    volume_offset = 0
-    source = ""
+        # ... (signal_time, is_travel, weather などの入力は以前のコードと同じ) ...
+        weather = input("天気 (Clear, Clouds, Rain, Snow)\n>>").split(",")
 
-    # データの集約
-    track_data = {
-        "id": db_track['id'], # これがあるから UPDATE できる
-        "type": type_input,
-        "time": time_list,
-        "signal_time":signal_time,
-        "is_travel":is_travel,
-        "is_travel_only":only_travel,
-        "weather": [w.strip() for w in weather if w.strip()],
-        "name": name,
-        "cover": cover,
-        "original": original,
-        "registered": registered,
-        "volume_offset":volume_offset,
-        "source":source,
-        "language":language,
+        # メタデータ推論
+        s_name, s_cover, s_original = name_suggestion(db_track['name'])
+        name = input(f"その曲は何と呼ばれている？ [{s_name}] >>") or s_name
+        cover = input(f"カバーした人はだれ？ [{s_cover}] >>") or s_cover
+        original = input(f"原曲をうたったのはだれ？ [{s_original}] >>") or s_original
 
-    }
+        today = datetime.now().strftime('%Y%m%d')
+        registered = input(f"登録日 [{today}] >>") or today
+        language = input(f"曲の言語[{db_track["language"]}]\n>>") or db_track["language"]
+        volume_offset = 0
+        source = ""
+        hour=25
 
-    # 登録実行
+        # データの集約
+        track_data = {
+            "id": db_track['id'], # これがあるから UPDATE できる
+            "type": type_input,
+            "time": time_list,
+            "signal_time":signal_time,
+            "is_travel":is_travel,
+            "is_travel_only":only_travel,
+            "weather": [w.strip() for w in weather if w.strip()],
+            "name": name,
+            "cover": cover,
+            "original": original,
+            "registered": registered,
+            "volume_offset":volume_offset,
+            "source":source,
+            "language":language,
+            "hour":hour,
+
+        }
+
+    elif type == "bgm":
+        print(f"\n--- [{index}/{total}] {db_track['name']} ---")
+
+        hour = input("何時台の曲?/n>>")
+
+        today = datetime.now().strftime('%Y%m%d')
+        registered = input(f"登録日 [{today}] >>") or today
+
+        volume_offset = 0
+
+        source = ""
+
+        track_data = {
+            "id": db_track['id'],
+            "type": type_input,
+            "time": ["none"],
+            "signal_time":None,
+            "is_travel":False,
+            "is_travel_only":False,
+            "weather": [],
+            "name": "",
+            "cover": "",
+            "original": "",
+            "registered": registered,
+            "volume_offset":volume_offset,
+            "source":source,
+            "language":"",
+            "hour":hour,
+
+        }
+
+
+
+        # 登録実行
     save_track_to_db(track_data)
 
 def save_track_to_db(data):
@@ -192,7 +231,8 @@ def save_track_to_db(data):
                             original=%s, 
                             registered=%s,
                             volume_offset=%s,
-                            language=%s
+                            language=%s,
+                            hour=%s
                         WHERE id=%s;
                     """
                     cur.execute(sql, (
@@ -209,6 +249,7 @@ def save_track_to_db(data):
                         data["volume_offset"],
                         data["language"],
                         data["id"],
+                        data["hour"]
                     ))
                 else:
                     # INSERT処理 (もし新規ファイルから登録する場合用)
@@ -297,6 +338,7 @@ def get_metadata(target_path,output_path):
         volume_offset = 0
         duration = 0
         source = ""
+        hour = 25
 
         suggested_name,suggested_cover,suggested_original = name_suggestion(path)
 
@@ -325,6 +367,7 @@ def get_metadata(target_path,output_path):
             "volume_offset":volume_offset,
             "duration":duration,
             "source":source,
+            "hour":hour,
         }
 
         register_track(track_data)
@@ -445,16 +488,17 @@ def get_duration(path):
     except Exception as e:
         return 0
     
+    
 
 if __name__ == "__main__":
 
-    mode = input("\n\n1.メタデータが入力されていない部分のみ入力する\n2.特定のメタデータを修正する\n3.時間帯や天気から曲を検索する\n4.ダウンロードする\n>>")
+    mode = input("\n\n1.メタデータが入力されていない部分のみ入力する\n2.データを出力する\n3.時間帯や天気から曲を検索する\n4.ダウンロードする\n>>")
     mode = int(mode)
     if mode == 1:
         create_tables()
         registration()
     elif mode == 2:
-        pass
+        output.output()
     elif mode == 3:
         pass
     elif mode == 4:
---
+++ output.py
@@ -0,0 +1,186 @@
+import os
+import psycopg2
+import json
+from psycopg2.extras import RealDictCursor
+import shutil
+
+config = {
+    "dbname":"test_61",
+    "user":"postgres",
+    "password":os.environ.get("PSQL_PASSWORD"),
+    "host":"localhost",
+    "port":5432,
+}
+
+setting = {#Settings.jsonにはこれが出力される
+    "quality":"p480",
+    "SceneSelect": True,
+    "SceneSelectRange": 0,
+    "betweenSongs": 6000,
+    "timezone": 9
+}
+
+filter_command = "SELECT * FROM tracks ORDER BY id;"
+
+
+"""
+SELECT * FROM tracks WHERE (name,signal,coverなど) = '(ほしい文字列など)'
+"""
+
+class output():
+    def __init__(self):
+
+        while True:
+            self.output_path = input("出力する先のファイルは\n>>")
+            if os.path.exists(self.output_path):
+                break
+
+        
+        self.quality = input("音声ファイルの質は？\n1.original\n2.480p\n3.audio only\n>>")
+        self.quality = int(self.quality)
+
+        self.quality = ["path_original","path_480p","path_audio"][self.quality-1]
+        
+
+        self.start_output()
+
+    def fetch_tracks(self):
+        conn = None
+        try:
+            conn = psycopg2.connect(**config)
+            cur = conn.cursor(cursor_factory = RealDictCursor)
+
+            query = filter_command
+
+            print(f"実行するクエリ\n{query}")
+
+            cur.execute(query)
+            rows = cur.fetchall()
+
+            print(f"{len(rows)}個のファイルを取得")
+
+            tracks = []
+            for row in rows:
+                track = self.convert_track_to_json(row)
+                tracks.append(track)
+
+            cur.close()
+            return tracks
+        
+        except psycopg2.Error as e:
+            print(F"Error:{e}")
+            return []
+        
+        finally:
+            if conn:
+                conn.close()
+
+    def export_to_json(self,tracks,output_path):
+        try:
+            with open(os.path.join(output_path,"Statics.json"),"w",encoding="utf-8") as f:
+                json.dump(tracks,f,ensure_ascii=False,indent=2)
+
+            with open(os.path.join(output_path,"Settings.json"),"w", encoding="utf-8") as f:
+                json.dump(setting,f,ensure_ascii=False,indent=2)
+            print(f"{output_path}に出力しました")
+            return True
+        except Exception as e:
+            print(f"Json export error:{e}")
+            return False
+
+
+    def convert_track_to_json(self,row):
+        track_type = row["type"]
+
+        if track_type == "music":
+
+            print(row["time"])
+
+            time_list = {
+                "lateNight":[0,1,2,3,4],
+                "earlyMorning":[5,6],
+                "morning":[7,8,9],
+                "lateMorning":[10,11,12],
+                "earlyAfternoon":[13,14,15],
+                "lateAfternoon":[16,17],
+                "earlyNight":[18,19,20],
+                "night":[21,22,23],
+                "all":[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23],
+                "none":[]
+            }
+
+            new_time = []
+            for data in row["time"]:
+                if data in time_list:
+                    new_time+=time_list.get(data)
+                else:
+                    new_time.append(int(data))
+
+            return {
+                "type":"music",
+                "path":row[str(self.quality)],
+                "name":row["name"],
+                "cover":row["cover"],
+                "original":row["original"],
+                "time":new_time,
+                "signal":row["signal_time"],
+                "travel":row["is_travel"],
+                "travelOnly":row["is_travel_only"],
+                "weather":row["weather"],
+                "registered":row["registered"],
+                "duration":row["duration"],
+                "source":row["source"],
+                "language":row["language"]
+            }
+        
+        elif track_type == "bgm":
+            return {
+                "type":"bgm",
+                "hour":row["hour"],
+                "path":row["path_audio"],
+                "registered":row["registered"],
+                "duration":row["duration"],
+                "source":row["source"]
+            }
+        
+        else:
+            print(f"unexpected track type:{track_type}")
+            print(1/0)
+
+
+    def copy_files(self,tracks):
+        music_path = os.path.join(self.output_path,"Music")
+        bgm_path = os.path.join(self.output_path,"BGM")
+
+        if not os.path.exists(music_path):
+            os.makedirs(music_path)
+
+        if not os.path.exists(bgm_path):
+            os.makedirs(bgm_path)
+
+
+        for track in tracks:
+            source_path = track["path"]
+            if track["type"] == "music":
+                if os.path.isfile(source_path):
+                    shutil.copy2(source_path,music_path)
+            elif track["type"] == "bgm":
+                if os.path.isfile(source_path):
+                    shutil.copy2(source_path,bgm_path)
+
+    def start_output(self):
+        if not os.environ.get("PSQL_PASSWORD"):
+            print("環境変数PSQL_PASSWORDが設定されていません")
+            return
+        
+        print("Start Exporting")
+        print(filter_command)
+
+        tracks = self.fetch_tracks()
+
+        if tracks:
+            self.export_to_json(tracks,self.output_path)
+            self.copy_files(tracks)
+
+        else:
+            print("トラックが見つかりませんでした")
